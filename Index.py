532f2b4df056496e9f05764d30e8075033d2d9cd
Adarsh Kumar <adarshchauhan0002@gmail.com>
2026-09-10 13:17:13 +0530

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,64 @@
+# age =25 
+# print(type(age))
+# name = "Damodar" 
+# print (type (name))
+# salary =1000000.50
+# print(type(salary))
+# 10+20
+# print (10+20)
+# "10"+"20"
+# print("10"+"20")
+
+# a = 10
+# b = 3
+# print (a+b)
+# print (a-b)
+# print (a*b)
+# print (a%b)
+# print ( a/b)
+# print (a//b)
+# print (a**b)
+# z = 2 + 3j
+# print (type(z)) 
+# print ()
+# students = 50
+# print (students)
+# # print(type(students))
+# boys = 20
+# girls = 30
+# print(boys +girls)
+
+# price = 99.99
+# print("price")
+# print (price)
+# print (type(price))
+
+# distance = 12.5
+# travelled =distance*2
+# print(travelled)
+
+# z = 2+ 3j
+# print(z)
+# print(type(z))
+# a = 3+4j
+# b=6+3j
+# # print(a+b)
+
+# is_logged_in =True
+# print(is_logged_in)
+# print (type (is_logged_in))
+# age =20 
+# print(age>=18)
+# print(type(age>=18))
+
+# name = "Adarsh chauhan"
+# print(name)
+# print(type(name))
+# salary = 20000000
+# print(salary)
+# print(type(salary))
+# print("salary")
+
+rajput = "chauhan"
+print(rajput)
+print(type(rajput))
