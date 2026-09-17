9fb494e27f4e5300f6d9cadd2408d8887b6cb1cb
Adarsh Kumar <adarshchauhan0002@gmail.com>
2026-09-17 15:15:21 +0530

    Remove commented-out code and print statements

--- Index.py
+++ Index.py
@@ -63,3 +63,192 @@ rajput = "chauhan"
 print(rajput)
 print(type(rajput))
 print(rajput)
+# if True:
+#     print("hello")
+
+# if True:
+#     print("A")  
+#     print("B")  
+
+# username ="none"
+# print(type("none"))
+# print(username)
+
+# api_response = None
+# if api_response is None:
+#     print("Failed to fetch data from the server.") 
+# else:
+#     print("data recieved succesfully")
+
+# age = 24
+# print(age)
+# print(type(age))
+
+# name="adarsh"
+# print(name)
+# print(type(name))
+
+# price=45.50
+# print(price)
+# print(type(45.50))
+    
+# x=10
+# y=20
+# z=30
+# # print(x+y+z)
+# a =10
+# b=10
+# if a is b:
+#     print("y")
+# else:
+#     print*("n")
+
+# def a(b ,c):
+#     print(b+c)
+# a(10,10)
+
+# a ="quad"
+# print(len(a))
+
+# a = "quad"
+# b ="AI"
+# print(len(a))
+# print(a, " +",b)
+# print(a.replace("q" ,"K"))
+# print(a.lower())
+# print(a.upper())
+
+
+# first = "python"
+# second = "coding"
+# result = first +" " + second
+# print(result) 
+
+# first_name ="Alice"
+# Last_name = "Smith"
+# full_name =first_name+ " " + Last_name
+# print(full_name)
+
+# a = []
+# print(a)
+# b =[1,2,3,4,]
+# print(b)
+# print(b[0])
+# print(b[2])
+
+# a =[10,20,30,40,50]
+# print(a[0:1])
+# print(a[1:3])
+# print(a[:3])
+# print(a[3:])
+# print(a[::-1])
+
+# a =[1,2,3]
+# a.append(4)
+# print(a)
+
+# b=[3,4,5,67,8,]
+# b.append(0)
+# print(b)
+
+# a=[10,20,30]
+# a.extend([45,50])
+# print(a)
+
+# b=[1,2,3,4,5,6]
+# b.extend([6,7,8])
+# print(b)
+
+# a =[10,20,30,40]
+# a.insert(1,60)
+# print(a)
+# a.remove(20)
+# print(a)
+
+# a=[10,20,30]
+# x=a.pop()
+# print(x)
+# print(a)
+# x=a.pop(0)
+# print(x)
+# print(a)
+# a.insert(10,20)
+# print(a)
+
+# # a =[1,2,3,4]
+# # a.clear()
+# # print(a)
+
+# a =[10,20,30,40]
+# print(a.index(30))
+# print(a.index(40))
+
+# a =[10,20,30,40,30,20,40,20]
+# print(a.count(20))
+# print(a.count(40))
+# print(a.count(30))
+# print(a.count(10))
+
+# a=[1,7,5,8,3,2,6]
+# a.sort()
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+# a.reverse()
+# print(a)
+# a.copy()
+# print(a)
+
+# a =[1,2]
+# b=[3,4]
+# print(a+b)
+# c =[6,8]
+# d=[9,10]
+# print(c+d)
+
+# a=[1,2]
+# print(a*3)
+# print(2 in a)
+# print(2 not in a)
+# print (1 in a)
+# print(0 in a)
+
+# a=(10,20,30,40)
+# print(a)
+# print(type(a))
+
+# b=(10)
+# print(type(b))
+
+# c=(10,)
+# print(type(c))
+
+# t=()
+# print(type(t))
+
+# t =tuple("python")
+# print(t)
+
+# a =("python","java","c++","sql")
+# print(a[-3])
+# print(a[10])
+
+# numbers =tuple([1,2,3,4])
+# print(numbers[0:3])
+# print(type(numbers))
+# print (numbers[:4])
+# print(numbers[2:])
+# print(numbers[:])
+
+# t=(10,20,30)
+
+# del t
+
+# print(t) 
+
+# adarsh =(1,2,3,4,2,4,3,)
+# print(adarsh.count(2))
+# print(adarsh.count(3))
+# print(adarsh.index(3))
+
+
